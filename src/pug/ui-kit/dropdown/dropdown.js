86c0c4b61f402d95edab5ff50373798c4d69eb15
muromtsev <t4k.org@mail.ru>
2020-04-07 19:56:20 +0300

    added counter in the dropdown.js

--- src/pug/ui-kit/dropdown/dropdown.js
+++ src/pug/ui-kit/dropdown/dropdown.js
@@ -5,16 +5,58 @@ let dropdown_menu = document.querySelector('.dropdown__menu');
 dropdown_link.addEventListener('click', (evt) => {
     evt.preventDefault();
     dropdown_number_of_guests.classList.toggle('dropdown-active');
-    dropdown_menu.classList.toggle('dropdown__menu-active');
+    // dropdown_menu.classList.toggle('dropdown__menu-active');
 });
 
-dropdown_menu.addEventListener('click', (evt) => {    
-    let target = evt.target;
+
+
+function dropdown_guests() {      
+    let dropdown_text = document.querySelector('.dropdown__text');
+    let btns_minus = document.querySelectorAll('.btn-minus');
+    let btns_plus = document.querySelectorAll('.btn-plus');
+    let baseNumbers = document.querySelectorAll('.count-number');
+    let txt = '';  
+        for(let i = 0; i < 3; i++) {
+
+            let num = Number(baseNumbers[i].textContent);
+
+            btns_minus[i].addEventListener('click', () => {
+                if(num == 0) {
+                    num = 0;                    
+                } else {
+                    num--;
+                    baseNumbers[i].innerHTML = num;
+                    txt = checkdeGuests(num);
+                }
+                dropdown_text.innerHTML = `${num} ${txt}`;
+            })
+
+            btns_plus[i].addEventListener('click', () => {
+                if(num >= 10) {
+                    num = 10;
+                } else {
+                    num++;
+                    baseNumbers[i].innerHTML = num;
+                    txt = checkdeGuests(num);      
+                }
+                dropdown_text.innerHTML = `${num} ${txt}`;
+            })
+        }
+
     
-    if(target.className == 'btn-minus') {        
-        console.log(target.nextSibling.textContent);
+}
+dropdown_guests()
+
+function checkdeGuests(item) {
+    let t = '';
+    if(item == 1) {
+        t = 'гость';
+    } else if(2 <= item >= 4) {
+        t = 'гостя';
+    } else {
+        t = 'гостей';
     }
-    else if(target.className == 'btn-plus') {       
-        console.log(target.previousSibling.textContent);
-    }
-})
+    return t;
+}
+
+dropdown_guests()
