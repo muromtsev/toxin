aa23957ef9b4d45532b09f17b30b890a9461d7ec
muromtsev <t4k.org@mail.ru>
2020-04-27 16:51:41 +0300

    create grid, responsive form-elems

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
 import $ from 'jquery';
 import '../sass/style.scss';
 import '../pug/ui-kit/range-slider/range-slider.js';
+import '../pug/ui-kit/checkbox-list/checkbox-list.js';
+import '../pug/ui-kit/like-button/like-button.js';
