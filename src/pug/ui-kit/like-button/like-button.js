3150fd085517a444a63cfe5bd755b08fe5973a3c
muromtsev <t4k.org@mail.ru>
2020-05-11 00:33:00 +0300

    fixed like-buttons

--- src/pug/ui-kit/like-button/like-button.js
+++ src/pug/ui-kit/like-button/like-button.js
@@ -2,33 +2,28 @@ function likeButton() {
     let btns = document.querySelectorAll('.js-like-button');
     
     btns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            
+        btn.addEventListener('click', () => { 
             changeClass(btn);
-            changeCount(btn);
-
         })
-    });        
-    
-    function changeCount(elem) {
-        let count = elem.querySelector('.js-like-button__count');
-        let idx = Number(count.innerHTML);
-            idx++;
-            count.innerHTML = idx;
-    };
+    });      
     function changeClass(elem) {
-        let icon = elem.querySelector('.js-like-button__heart').firstElementChild;
-        let txt = '';
+        let icon = elem.querySelector('.js-like-button__heart').firstElementChild,
+            count = elem.querySelector('.js-like-button__count'),
+            idx = Number(count.innerHTML),
+            txt = '';
         
         if(elem.classList.contains('js-like-button__active')) {
             elem.classList.remove('js-like-button__active');
             txt = 'favorite_border';
+            idx = idx - 1;
         }
         else {
             elem.classList.add('js-like-button__active');
             txt = 'favorite';
+            idx = idx + 1;
         }
         icon.innerHTML = txt;
+        count.innerHTML = idx;
     }
 }
 likeButton();
