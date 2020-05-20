fe35301e360ebc8652e8c83c1de22b6113f2e30a
muromtsev <t4k.org@mail.ru>
2020-05-20 17:08:32 +0300

    create first card, fixed component card

--- src/pug/ui-kit/checkbox-list/checkbox-list.js
+++ src/pug/ui-kit/checkbox-list/checkbox-list.js
@@ -1,10 +1,17 @@
-let checkbox_menu = document.querySelector('.checkbox-list__menu');
-let checkboxArrow = document.querySelector('.checkbox__arrow');
-checkboxArrow.addEventListener('click', function(event) {
-    event.preventDefault();    
-    let labels = document.querySelectorAll('.checkbox-list__menu .check');
-    checkbox_menu.classList.toggle('menu-active');
-    this.classList.toggle('arrow-active');    
-})
+document.addEventListener('DOMContentLoaded', () => {
+    let checkbox_menu = document.querySelector('.checkbox-list__menu');
+    let checkboxArrow = document.querySelector('.checkbox__arrow');
+
+    if(checkbox_menu && checkboxArrow) {
+        checkboxArrow.addEventListener('click', function(event) {
+            event.preventDefault();    
+            let labels = document.querySelectorAll('.checkbox-list__menu .check');
+            checkbox_menu.classList.toggle('menu-active');
+            this.classList.toggle('arrow-active');    
+        })
+    }
+    
+});
+
 
 
